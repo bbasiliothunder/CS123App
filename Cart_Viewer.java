c75f87eec517b498af5ab2964723cdf9e28f9be0
Jino <sonnysinzanity@gmail.com>
2015-10-11 15:54:48 +0800

    Java File for Cart Viewer XML

---
+++ Cart_Viewer.java
@@ -0,0 +1,15 @@
+package cs123.app.restoandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Cart_Viewer extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cart__viewer);
+	}
+}
