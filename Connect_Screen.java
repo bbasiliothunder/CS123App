469e8707de3808aa578d2d0a248b943a1784fc83
Jino <sonnysinzanity@gmail.com>
2015-10-11 15:55:38 +0800

    Java file for Connect Screen XML

---
+++ Connect_Screen.java
@@ -0,0 +1,15 @@
+package cs123.app.restoandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Connect_Screen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect__screen);
+    }
+}
